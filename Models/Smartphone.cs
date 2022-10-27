bfc3eda34b92487f63a4c32943e3a168b3b01273
Gabriel de Paula Souza <gaahlisboa@gmail.com>
2022-10-27 19:40:16 -0300

    Sistem up

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,29 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
-        public void Ligar()
+        public void Ligar(string numero)
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando para {numero}");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação anônima...");
+        }
+        public void ReceberLigacao(string numero)
+        {
+            Console.WriteLine($"Recebendo ligação de {numero}");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("988006655", "NOKIA M2", "001970-01-010000-0", 64);
+Console.WriteLine("INFORMAÇÕES SOBRE O CELULAR:");
+Console.WriteLine(
+    $"Número: {nokia.Numero}\n" +
+    $"Modelo: {nokia.Modelo}\n" +
+    $"IMEI: {nokia.IMEI}\n" +
+    $"Memória: {nokia.Memoria} GB");
+nokia.Ligar("40028922");
+nokia.ReceberLigacao();
+
+Console.WriteLine("\n------------------------\n");
+
+Iphone iphone = new Iphone("982449746", "IPHONE X", "001970-01-010000-1", 128);
+Console.WriteLine("INFORMAÇÕES SOBRE O CELULAR:");
+Console.WriteLine(
+    $"Número: {iphone.Numero}\n" +
+    $"Modelo: {iphone.Modelo}\n" +
+    $"IMEI: {iphone.IMEI}\n" +
+    $"Memória: {iphone.Memoria} GB");
+iphone.Ligar("40028922");
+iphone.ReceberLigacao("39290304");
